8431787959dbe0d6e1326fa4f672c9ef8be4f20f
Felipe Luzzardi da Rosa <fldrosa@inf.ufpel.edu.br>
2020-08-23 01:22:35 -0300

    Dashboard interface added

---
+++ components/sheets/NaverProfile.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <v-img
+      src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+      alt="Foto do naver"
+    ></v-img>
+    <div class="mt-4">
+      <p class="name" :style="{ fontSize: size }">Juliano Reis</p>
+      <p :style="{ fontSize: size }">Front-end Developer</p>
+      <v-icon :large="$vuetify.breakpoint.mdAndUp" class="mr-2" color="primary"
+        >mdi-delete</v-icon
+      >
+      <v-icon :large="$vuetify.breakpoint.mdAndUp" color="primary"
+        >mdi-pencil</v-icon
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isHydrated: false,
+    }
+  },
+  computed: {
+    size() {
+      if (this.isHydrated && this.$vuetify.breakpoint.mdAndUp) {
+        return '25px'
+      }
+      return '16px'
+    },
+  },
+  mounted() {
+    this.isHydrated = true
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+  margin-bottom: 0;
+}
+.name {
+  font-weight: 600;
+}
+</style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,3 +1,36 @@
 <template>
-  <div>TESTE</div>
+  <div class="px-6 mt-12">
+    <!-- Header -->
+    <div class="d-flex flex-column flex-sm-row align-center">
+      <span class="page-title mb-0 mb-4">Navers</span>
+      <v-spacer></v-spacer>
+      <v-btn depressed tile color="button" class="pa-6 white--text"
+        >Adicionar Naver</v-btn
+      >
+    </div>
+    <!-- List of profiles -->
+    <v-row class="mt-4">
+      <v-col v-for="i in 4" :key="i" cols="6" sm="4" lg="3">
+        <NaverProfile></NaverProfile>
+      </v-col>
+    </v-row>
+  </div>
 </template>
+
+<script>
+import NaverProfile from '@/components/sheets/NaverProfile.vue'
+
+export default {
+  components: {
+    NaverProfile,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.page-title {
+  font-weight: 600;
+  font-size: 40px;
+  line-height: 48px;
+}
+</style>
